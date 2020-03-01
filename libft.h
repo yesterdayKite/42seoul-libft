ab029aca4911e6d226db10f4dcc8b8e48d8ecbff
jae <jae@student.42seoul.kr>
2020-03-01 21:35:47 +0900

    ft_is*.c & ft_to*.c

--- libft.h
+++ libft.h
@@ -16,6 +16,12 @@
 
 size_t		strlcat(char	*restrict dst, const char	*restrict src, size_t	dstsize);
 size_t		ft_strlen(const char *s);
+char		*ft_strrchr(const char *s, int c);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char		*ft_strchr(const char *s, int c);
+int			ft_isacii(int c);
+int			ft_isdigit(int c);
 
 
 #endif

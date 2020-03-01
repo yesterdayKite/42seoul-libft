c5dda20c973c45dc1a010ee793edfbefcff9584a
jae <jae@student.42seoul.kr>
2020-03-01 20:05:11 +0900

    second push

---
+++ ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jae <jae@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/01 17:08:25 by jae               #+#    #+#             */
+/*   Updated: 2020/03/01 17:08:25 by jae              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t		strlcat(char	*restrict dst, const char	*restrict src, size_t	dstsize)
+{
+	size_t	dst_cnt;
+	size_t	src_cnt;
+	size_t	i;
+
+	src_cnt = 0;
+	dst_cnt = 0;
+	i = 0;
+
+	while (src[src_cnt] != '\0')
+		src_cnt++;
+	while (dst[dst_cnt] != '\0')
+		dst_cnt++;
+	if (dstsize <= dst_cnt)
+		src_cnt += dstsize;
+	else
+		src_cnt += dst_cnt;
+	while (src[i] != '\0' && i + 1 < dstsize)
+		dst[dst_cnt++] = src[i++];
+	dst[dst_cnt] = '\0';
+	return (src_cnt);
+}
---
+++ libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jae <jae@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/01 18:10:13 by jae               #+#    #+#             */
+/*   Updated: 2020/03/01 18:10:13 by jae              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stddef.h>
+
+size_t		strlcat(char	*restrict dst, const char	*restrict src, size_t	dstsize);
+size_t		ft_strlen(const char *s);
+
+
+#endif

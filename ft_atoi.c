74c5e093270e969b040cd654daa8e3c4910720fb
jae <jae@student.42seoul.kr>
2020-03-01 22:01:20 +0900

    ft_atoi.c

---
+++ ft_atoi.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jae <jae@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/01 21:36:35 by jae               #+#    #+#             */
+/*   Updated: 2020/03/01 21:36:35 by jae              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	res;
+	int	is_negative;
+	int i;
+
+	is_negative = 0;
+	res = 0;
+	i = 0 ;
+	while (str[i] && (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' ||
+			str[i] == '\v' || str[i] == '\f' || str[i] == '\r'))
+		i++;
+	if (str[i] == '-')
+		is_negative++;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - 48);
+		i++;
+	}
+	if (is_negative % 2 == 0)
+		return (res);
+	else
+		return (res * -1);
+}

ee132b5bc774a4fedd89802d9e71de201907a43b
jae <jae@student.42seoul.kr>
2020-03-08 20:12:02 +0900

    string function

---
+++ ft_strdup.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jae <jae@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/06 12:51:05 by jae               #+#    #+#             */
+/*   Updated: 2020/03/06 12:51:05 by jae              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <string.h>
+#include <stdlib.h>
+
+char	*ft_strdup(const char	*src)
+{
+	char	*cpy;
+	cpy = NULL;
+	if(!(cpy = (char *)malloc((ft_strlen(src) + 1) * sizeof(char))))
+		return (NULL);
+	ft_strcpy(cpy, src);
+	return (cpy);
+}
---
+++ ft_strjoin.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jae <jae@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/07 19:39:20 by jae               #+#    #+#             */
+/*   Updated: 2020/03/07 19:39:20 by jae              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	size_t		s1_len;
+	size_t		s2_len;
+	int			i;
+	char		*res;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	if(!(res = malloc(sizeof(char) * (s1_len + s2_len + 1))))
+		return (NULL);
+	i = -1;
+	while (++i < s1_len)
+		res[i] = s1[i];
+	while (++i - s1_len < s2_len)
+		res[i] = s2[i -s1_len];
+	res[i] = '\0';
+	return (res);
+}
